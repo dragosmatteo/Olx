37dc6599d26de669c6b53b3125b4b22aefe2176e
Dragoș Matteo <dragosmatteod@gmail.com>
2021-05-21 19:14:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const navbar =  document.querySelector('.section--1');
 let sticky = navbar.getBoundingClientRect().height;
 
+
+// !! Sticky navigation
+
 const navbarScroll = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky')
