3b7d71ebff960c0ba824a81ca62950211d0efcdc
Dragoș Matteo <dragosmatteod@gmail.com>
2021-05-21 18:58:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // !! Dragos Matteo 20.05.2021 OLX 
 
+const navbar =  document.querySelector('.section--1');
+let sticky = navbar.getBoundingClientRect().height;
+
+const navbarScroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky');
+  }
+};
+
+window.onscroll = navbarScroll;
