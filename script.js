b311827cae0714b45b89537a5377e27a954bbf0f
Dragoș Matteo <dragosmatteod@gmail.com>
2021-05-31 18:02:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // !! Dragos Matteo 20.05.2021 OLX 
 
 const navbar =  document.querySelector('.section--1');
+const input = document.getElementById('searchTxt').value;
+const cauta = document.getElementById('caut');
 let sticky = navbar.getBoundingClientRect().height;
 
 
@@ -14,3 +16,7 @@ const navbarScroll = () => {
 };
 
 window.onscroll = navbarScroll;
+
+if (input.length > 0) {
+  document.getElementById('caut').style.backgroundColor = '#002f34';
+}
